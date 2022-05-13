5cd4f190f6e866d944ea0cb0357a94c93444ae09
emir4ik-code <emirahmetov@gmail.com>
2022-05-13 10:53:10 -0400

    more auth work

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const cors = require("cors");
+
+
+app.use(cors());
+app.use(express.json());
 
 const PORT = 3000; 
+
+app.use("/auth", require("./routes/jwtAuth"))
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`)
---
+++ routes/jwtAuth.js
@@ -0,0 +1,59 @@
+const pool = require("../db")
+const router = require("express").Router();
+const bcryt = require("bcrypt")
+const jwtGenerator =  require("../util/jwtGenerator")
+
+
+console.log(pool)
+
+router.post("/register", async(req, res) => {
+    try {
+        const {email, password, username} = req.body; 
+
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email])
+        
+        if(user.rows.length !== 0) {
+            return res.status(401).send("user already exists")
+        }
+        
+        const saltRounds = 10;
+        const salt = await bcryt.genSalt(saltRounds)
+
+        const bcrytPassword = await bcryt.hash(password, salt)
+
+        const newUser = await pool.query("INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING *", [email, username, bcrytPassword])
+
+        const token = jwtGenerator(newUser.rows[0].id)
+        res.json({token})
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send("server error")
+    }
+})
+
+router.post("/login", async(req, res) => {
+    try {
+        const {email, password} = req.body; 
+
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email])
+
+        if(user.rows.length === 0) {
+            res.status(401).json("password or email is incorrect")
+        }
+
+        const validPassword = await bcryt.compare(password, user.rows[0].password);
+
+        if(!validPassword) {
+            res.status(401).json("password or email is incorrect")
+        }
+        const token = jwtGenerator(user.rows[0].id)
+        
+        res.json({token})
+
+    } catch (error) {
+        
+    }
+})
+
+module.exports = router;
